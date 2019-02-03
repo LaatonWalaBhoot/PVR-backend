974142c7fdbab8bb8d327547328becb902f2b858
Apple <apple@LaatonWalaBhoot.local>
2019-02-03 20:59:23 +0530

    first app configuration

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(compression());
 
+mongoose.connect(config.get('db'), {useNewUrlParser: true})
+    .then(() => console.log('Connected to MongoDb...'))
+    .catch(err => console.log('Error: ', err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
+
