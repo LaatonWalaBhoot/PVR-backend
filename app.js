520aaf978fae4319f6edf242ea030101ef5c191b
Apple <apple@LaatonWalaBhoot.local>
2019-02-03 20:48:56 +0530

    added environ-variable support

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+const config = require('config');
+
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
 
